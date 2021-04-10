12969cb343d285531731b8f1585e09dd4c758094
Jure Mihevc <juremihevcster@gmail.com>
2021-04-10 23:40:07 +0200

    Add FizzBuzz argument and define range from 1 to n + 1 inside the range function

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,15 +1,17 @@
+print("FIZZBUZZ")
+
 num_sel = int(input("Select a number beetween 1 an 100: "))
 
-x = range(num_sel)
+for n in range(1 , num_sel + 1):
 
-for n in x:
+    if n % 3 == 0 and n % 5 == 0:
+        print("fizzbuzz")
 
-    if (n + 1) % 3 == 0:
+    elif n % 3 == 0:
         print("fizz")
 
-    elif (n + 1) % 5 == 0:
+    elif n % 5 == 0:
         print("buzz")
 
     else:
-        print(n + 1)
-
+        print(n)
